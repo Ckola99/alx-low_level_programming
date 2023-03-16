ccab7be7b81d8c36895bbdf38ca00c2bd5e67cfa
Ckola99 <christopherkola@gmail.com>
2023-03-16 11:42:24 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,17 +9,15 @@
 int main(void)
 {
 	char Char;
-	int Int; 
+	int Int;
 	long int Longint;
 	long long int Longlong;
 	float Float;
-
-
+	
 	printf("Size of a char: %zu byte(s)\n", sizeof(Char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(Int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(Longint));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(Longlong));
 	printf("Size of a float: %zu byte(s)\n", sizeof(Float));
-
 	return (0);
 }
