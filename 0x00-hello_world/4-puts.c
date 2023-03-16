5a0709395a63ce1fb0bfae7c086f5fa5c4f3a6c4
Ckola99 <christopherkola@gmail.com>
2023-03-16 07:06:45 -0700

    C language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+/**
+ * A function that prints text using puts function
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
