21fbfc26d7362572f9984d6e6b182c2531d5c7f0
Ckola99 <christopherkola@gmail.com>
2023-03-17 07:08:38 -0700

    Coding

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,9 +10,9 @@
 int main(void)
 {
 int i;
-char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n','o' ,'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char alpha[26] = {"abcdefghijklmnopqrstuvwxyz"};
 
-for (i = 0; i < 26; i++) 
+for (i = 0; i < 26; i++)
 {
 	putchar(alpha[i]);
 }
