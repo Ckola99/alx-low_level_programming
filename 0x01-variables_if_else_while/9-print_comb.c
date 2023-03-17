177a10bdc1027f4139be40e4203bafa2c968812b
Ckola99 <christopherkola@gmail.com>
2023-03-17 12:45:39 -0700

    correction of #9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,11 +16,11 @@ for (num = 0; num < 10; num++)
 putchar(num + '0');
 if (num == 9)
 {
-putchar(' ');
+putchar('\n');
 return (0);
 }
 putchar(',');
-putchar('\n');
+putchar(' ');
 }
 
 return (0);
