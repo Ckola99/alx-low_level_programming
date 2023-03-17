46faada4791b9de7668bea395b8a8c34b124e439
Ckola99 <christopherkola@gmail.com>
2023-03-17 14:32:10 -0700

    correction #11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,17 +13,17 @@ int main(void)
     int num2 = 0;
     int num3 = 0;
 
-    for(num1 = 0; num1 < 8; num1++)
+    for (num1 = 0; num1 < 8; num1++)
     {
-        for(num2 = num1 + 1; num2 < 9; num2++)
+        for (num2 = num1 + 1; num2 < 9; num2++)
         {
-            for(num3 = num2 + 1; num3 < 10; num3++)
+            for (num3 = num2 + 1; num3 < 10; num3++)
             {
                 putchar(num1 + '0');
                 putchar(num2 + '0');
                 putchar(num3 + '0');
 
-                if(num1 == 7 && num2 == 8 && num3 == 9)
+                if (num1 == 7 && num2 == 8 && num3 == 9)
                 {
                     return (0);
                 }
@@ -33,5 +33,5 @@ int main(void)
             }
         }
     }
-    return(0);
+    return (0);
 }
