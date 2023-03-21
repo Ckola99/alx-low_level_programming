dc4b0be6b98bd6b464088b34a46f6439358e7b88
CKola <christopherkola@gmail.com>
2023-03-21 11:14:40 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of number.
+ * @n: is the number to be evaluated.
+ * Return: 1 if positive, -1 if negative and 0 if 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar(45);
+		return (-1);
+	}
+	else
+	{
+		putchar(48);
+		return (0);
+	}
+}
