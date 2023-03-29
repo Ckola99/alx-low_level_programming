4590d0a179f50d6942f80d0b31592dcc5da1a535
Ckola99 <Christopherkola@gmail.com>
2023-03-29 12:40:31 +0200

    .\9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * _strcpy -prints elements of an array of integers, followed by a new line.
@@ -7,14 +8,38 @@
  * @src: is the pointer to the char.
  * Return: pointer to dest.
  */
+int main(void)
+{
+    char src[98];
+    char *dest;
 
+    dest = _strcpy(src, "First, solve the problem. Then, write the code\n");
+    printf("%s", src);
+    printf("%s", dest);
+    return (0);
+}
 char *_strcpy(char *dest, char *src)
 {
         int i;
+        i= 0;
+        int n;
+        n = strlen(src);
 
-        for (i = 0; src[i] < '\0'; i++)
+        if (src == NULL || dest == NULL)
+        {
+                return dest;
+        }
+
+        while (i < n && src[i])
         {
                 dest[i] = src[i];
+                i++;
+        }
+
+        while (i < n)
+        {
+                dest[i] = '\0';
+                i++;
         }
 
         return (dest);
