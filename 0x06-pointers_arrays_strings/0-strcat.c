8931430bf3676989d61b47a4cf8c67793d7a3022
Ckola99 <Christopherkola@gmail.com>
2023-03-31 02:06:46 +0200

    .\0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,20 +9,6 @@
  * @src: is the second pointer to the string to be appended.
  * Return: pointer to reulting string dest.
  */
-int main(void)
-{
-        char s1[98] = "Hello ";
-        char s2[] = "World!\n";
-        char *ptr;
-
-        printf("%s\n", s1);
-        printf("%s", s2);
-        ptr = _strcat(s1, s2);
-        printf("%s", s1);
-        printf("%s", s2);
-        printf("%s", ptr);
-        return (0);
-}
 
 char *_strcat(char *dest, char *src)
 {
