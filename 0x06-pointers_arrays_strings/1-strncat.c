6a2f8a11634627bc24f0ea6a75073a4f5e05e3a9
Ckola99 <Christopherkola@gmail.com>
2023-03-31 09:12:29 +0200

    .\1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,33 +10,14 @@
  * @dest: 2nd input pointer.
  * Return: pointer to new string.
  */
-int main(void)
-{
-    char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
-}
+
 char *_strncat(char *dest, char *src, int n)
 {
         int i;
-        int length;
         int len_dest;
-        int len_src;
 
         len_dest = strlen(dest);
-        len_src = strlen(src);
 
         if (n > 0)
         {
