5a1ef4d7bef974754db62d58520b9b848974368b
Ckola99 <Christopherkola@gmail.com>
2023-03-31 14:32:21 +0200

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,22 +13,23 @@
 
 int _strcmp(char *s1, char *s2)
 {
+        int i;
 
-        while (*s1 != '\0' && *s2 != '\0')
+        for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
         {
-                if (*s1 == *s2)
+                if (s1[i] == s2[i])
                 {
                         return (0);
                 }
 
-                if (*s1 < *s2)
+                if (s1[i] < s2[i])
                 {
-                        return (*s1 - *s2);
+                        return (s1 - s2);
                 }
 
-                if (*s1 > *s2)
+                if (s1[i] > s2[i])
                 {
-                        return (*s1 - *s2);
+                        return (s1 - s2);
                 }
         }
         return (0);
