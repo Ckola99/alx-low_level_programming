6397d6d5defe7352ed82cedc5822258ddab9d426
Ckola99 <Christopherkola@gmail.com>
2023-03-31 14:35:06 +0200

    .\3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,6 +10,16 @@
  *         0 if s1 = s2
  *         Negative byte difference if s1 < s2
  */
+int main(void)
+{
+    char s1[] = "Hello";
+    char s2[] = "World!";
+
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
+}
 
 int _strcmp(char *s1, char *s2)
 {
@@ -24,12 +34,12 @@ int _strcmp(char *s1, char *s2)
 
                 if (s1[i] < s2[i])
                 {
-                        return (s1 - s2);
+                        return (s1[i] - s2[i]);
                 }
 
                 if (s1[i] > s2[i])
                 {
-                        return (s1 - s2);
+                        return (s1[i] - s2[i]);
                 }
         }
         return (0);
