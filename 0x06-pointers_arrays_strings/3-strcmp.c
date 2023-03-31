e0d1b55a9e39ffca9a7f872816b03447126c4adc
Ckola99 <Christopherkola@gmail.com>
2023-03-31 13:45:47 +0200

    fix 0

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 {
         int i;
 
-        while (*s1 && *s2 != '\0')
+        while (*s1 != '\0' && *s2 != '\0')
         {
                 if (*s1 == *s2)
                 {
@@ -31,6 +31,5 @@ int _strcmp(char *s1, char *s2)
                 {
                         return (*s1 - *s2);
                 }
-
         }
 }
