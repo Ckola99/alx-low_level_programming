5f94f433e5d834a53d111ec5d23a4e72cb8e583f
Ckola99 <Christopherkola@gmail.com>
2023-04-03 12:13:57 +0200

    fixes to 4 and README.md

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,13 @@
 void reverse_array(int *a, int n)
 {
         int i;
+
         for (i = 0; i <= n / 2; i++)
         {
                 int tmp;
 
                 tmp = a[i];
-                a[i] = a[n - i - 1];
-                a[n - i - 1] = tmp;
+                a[i] = a[n - 1 - i];
+                a[n - 1 - i] = tmp;
         }
 }
