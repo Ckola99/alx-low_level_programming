f05ce07d1271683a1d248ce49d0115e705381c2f
Ckola99 <Christopherkola@gmail.com>
2023-04-04 15:25:21 +0200

    number 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,11 +11,27 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int d1 = 0, d2 = 0;
-	for (i = 0; i < size * size; i += size + 1)
+	int j;
+
+	int d1 = 0,
+	d2 = 0;
+
+	for (i = 0; i < size; i++)
 	{
-		d1 += a[i];
-		d2 += a[i + size - 1];
+		for (j = 0; j < size; j++)
+		{
+
+			if (i == j)
+			{
+				d1 += a[i * size + j];
+			}
+
+			if (j == size - 1 - i)
+			{
+				d2 += a[i * size + j];
+			}
+		}
 	}
+
 	printf("%d, %d\n", d1, d2);
 }
