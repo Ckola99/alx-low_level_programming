3adeaacb525ba3fd5b38b1b14c30eb479a3f7107
Ckola99 <Christopherkola@gmail.com>
2023-04-04 13:46:56 +0200

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,16 +12,6 @@
  *         If the substring is not located - NULL.
  */
 
-int main(void)
-{
-	char *s = "hello, world";
-	char *f = "world";
-	char *t;
-
-	t = _strstr(s, f);
-	printf("%s\n", t);
-	return (0);
-}
 
 char *_strstr(char *haystack, char *needle)
 {
