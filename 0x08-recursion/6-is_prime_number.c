3e360090edcda0a4563921de82a1a6db4cb0c18f
Ckola99 <Christopherkola@gmail.com>
2023-04-05 22:02:20 +0200

    addition of functions

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,6 +3,55 @@
 #include <math.h>
 
 /**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
+ *
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (find_sqrt(n, 1));
+	}
+}
+
+/**
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: The number to find the square root of.
+ * @root: The root to be tested.
+ *
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
+ */
+
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+	{
+		return (root);
+	}
+	else if (root >= num / 2)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (find_sqrt(num, root + 1));
+	}
+}
+
+/**
  * is_prime_number - Checks if a number is prime.
  * @n: The number to be checked.
  *
@@ -13,8 +62,6 @@
 
 int is_prime_number(int n)
 {
-	int div = 2;
-
 	if (n <= 1)
 	{
 		return (0);
@@ -38,7 +85,7 @@ int is_prime_helper(int n, int div)
 		return (0);
 	}
 
-	if (div > sqrt(n))
+	if (div > _sqrt_recursion(n))
 	{
 		return (1);
 	}
