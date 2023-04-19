c9f3152fcc9ce015c1d717d7d51dbb3a4d621fc5
Ckola99 <Christopherkola@gmail.com>
2023-04-20 01:16:11 +0200

    updated 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_name - Prints a name.
@@ -13,6 +14,6 @@ void print_name(char *name, void (*f)(char *))
 	{
 		return;
 	}
-	
+
 	f(name);
 }
