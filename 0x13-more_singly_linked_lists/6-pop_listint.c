db57a937c3160552766a1df38e189aee5dfec705
Ckola99 <Christopherkola@gmail.com>
2023-05-04 02:26:58 +0200

    Task 0x13

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * pop_listint - Deletes the head node of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ *
+ * Return: If the linked list is empty - 0.
+ *         Otherwise - The head node's data (n).
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int num;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = (*head)->next;
+	num = (*head)->n;
+	free(*head);
+	*head = tmp;
+
+	return(num);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *                           list at a given position.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ *       node should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *temp;
+	unsigned int count;
+
+	new_node = malloc(sizeof(listint_t));
+	if(new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	temp = *head;
+	count = 0;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (temp)
+	{
+		if (count == (idx - 1))
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
+
+		}
+		count++;
+		temp = temp->next;
+	}
+	return (NULL);
+}
